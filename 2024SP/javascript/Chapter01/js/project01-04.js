d86b2b949f0a45be77d13ce3eb344248c40f3ede
Lirussell <166352311+Lirussell@users.noreply.github.com>
2024-04-07 22:07:00 -0500

    Add files via upload

---
+++ 2024SP/javascript/Chapter01/js/project01-04.js
@@ -0,0 +1,13 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+      Author: Liam Russell
+      Date:   02/18/2024
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+let homeStreet = "1 Main St.", homeCity = "Sicilia", homeState = "MA", homeCode = "02103";
+let workStreet = "15 Oak Ln.", workCity = "Central City", workState = "MA", workCode = "02104";
